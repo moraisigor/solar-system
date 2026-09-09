2cec8960bca5054fbe8f4baa01e452f4bd7d38b7
Igor Morais <igor.morais@hollowsoft.com>
2026-09-09 22:45:39 +0000

    add control clock

---
+++ app/src/scene/Clock/control.clock.tsx
@@ -0,0 +1,14 @@
+import { useFrame } from '@react-three/fiber'
+
+import { useClock } from './use.clock'
+
+export const ControlClock = () => {
+  const { time } = useClock()
+
+  useFrame((_, value) => {
+    // 1 second is equivalent to 1 hour
+    time.current += (1 / 24) * value
+  }, -1)
+
+  return null
+}
