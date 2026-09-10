28de912a782a9c98b9836f9b0ebf437ae5edbbf1
Igor Morais <igor.morais@hollowsoft.com>
2026-09-10 04:27:52 +0000

    add mercury information data

---
+++ app/src/astronomy/mercury.ts
@@ -0,0 +1,15 @@
+export const MERCURY = {
+  name: 'Mercury',
+  axis: 0.38709927,
+  orbit: 87.969257,
+  radius: 2439.4,
+  rotation: 58.6462,
+  obliquity: 2.11 / 60,
+  inclination: 7.00497902,
+  eccentricity: 0.20563593,
+  longitude: {
+    average: 252.2503235,
+    ascending: 48.33076593,
+    perihelion: 77.45779628
+  }
+} as const
