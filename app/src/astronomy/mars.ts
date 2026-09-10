0945d65723177b623345a5eabfb4e65dc00ee124
Igor Morais <igor.morais@hollowsoft.com>
2026-09-10 04:29:44 +0000

    add mars information data

---
+++ app/src/astronomy/mars.ts
@@ -0,0 +1,15 @@
+export const MARS = {
+  name: 'Mars',
+  axis: 1.52371034,
+  orbit: 686.98,
+  radius: 3389.5,
+  rotation: 1.02595676,
+  obliquity: 25.19,
+  inclination: 1.84969142,
+  eccentricity: 0.0933941,
+  longitude: {
+    average: -4.55343205,
+    ascending: 49.55953891,
+    perihelion: -23.94362959
+  }
+} as const
