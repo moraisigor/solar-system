3f8b0dd8b5be0c901e816bc1dcf1104192ad54e5
Igor Morais <igor.morais@hollowsoft.com>
2026-09-10 20:03:54 +0000

    add saturn information data

---
+++ app/src/astronomy/saturn.ts
@@ -0,0 +1,19 @@
+export const SATURN = {
+  name: 'Saturn',
+  axis: 9.53667594,
+  orbit: 10759.22,
+  radius: 58232,
+  rotation: 0.44401,
+  obliquity: 26.73,
+  inclination: 2.48599187,
+  eccentricity: 0.05386179,
+  ring: {
+    in: 66_900,
+    out: 136_775
+  },
+  longitude: {
+    average: 49.95424423,
+    ascending: 113.66242448,
+    perihelion: 92.59887831
+  }
+} as const
