ab7f4d61732083c19ee1c0796cd78902363adc55
Igor Morais <igor.morais@hollowsoft.com>
2026-09-10 04:29:28 +0000

    add jupiter information data

---
+++ app/src/astronomy/jupiter.ts
@@ -0,0 +1,15 @@
+export const JUPITER = {
+  name: 'Jupiter',
+  axis: 5.202887,
+  orbit: 4332.589,
+  radius: 69911,
+  rotation: 0.41354,
+  obliquity: 3.13,
+  inclination: 1.30439695,
+  eccentricity: 0.04838624,
+  longitude: {
+    average: 34.39644051,
+    ascending: 100.47390909,
+    perihelion: 14.72847983
+  }
+} as const
