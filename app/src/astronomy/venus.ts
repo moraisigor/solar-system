5199bcf07d517317d22db7241ef2be880f58f090
Igor Morais <igor.morais@hollowsoft.com>
2026-09-10 04:28:18 +0000

    add venus information data

---
+++ app/src/astronomy/venus.ts
@@ -0,0 +1,15 @@
+export const VENUS = {
+  name: 'Venus',
+  axis: 0.72333566,
+  orbit: 224.700799,
+  radius: 6051.8,
+  rotation: -243.018,
+  obliquity: 177.36,
+  inclination: 3.39467605,
+  eccentricity: 0.00677672,
+  longitude: {
+    average: 181.9790995,
+    ascending: 76.67984255,
+    perihelion: 131.60246718
+  }
+} as const
