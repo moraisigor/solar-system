6d4d7414f0f0bd2db9c81c8f563b98ef12cc89e1
Igor Morais <igor.morais@hollowsoft.com>
2026-09-11 12:46:03 +0000

    add kepler element

---
+++ app/src/type/kepler.element.ts
@@ -0,0 +1,11 @@
+export type KeplerElement = {
+  axis: number
+  orbit: number
+  eccentricity: number
+  inclination: number
+  longitude: {
+    average: number
+    ascending: number
+    perihelion: number
+  }
+}
