dbdf768cd587ec6990aea6b8448f42ef7b7ba124
Igor Morais <igor.morais@hollowsoft.com>
2026-09-12 15:17:24 +0000

    add jupiter scene component

---
+++ app/src/scene/Jupiter/index.tsx
@@ -0,0 +1,3 @@
+import { Jupiter } from './jupiter'
+
+export { Jupiter }
---
+++ app/src/scene/Jupiter/jupiter.tsx
@@ -0,0 +1,71 @@
+import { useRef, type FunctionComponent } from 'react'
+
+import { useFrame, useLoader } from '@react-three/fiber'
+import { SRGBColorSpace, TextureLoader, type Group, type Mesh } from 'three'
+
+import { useClock } from '@/scene/Clock'
+import { useFocusTarget } from '@/scene/Focus'
+import { Name } from '@/scene/Name'
+
+import { heliocentric, JUPITER, toSceneUnit } from '@/astronomy'
+import type { Vec } from '@/astronomy/kepler'
+
+const radius = toSceneUnit(JUPITER.radius)
+const rotation = (Math.PI * 2) / JUPITER.rotation
+const obliquity = (JUPITER.obliquity * Math.PI) / 180
+
+export const Jupiter: FunctionComponent = () => {
+  const { time } = useClock()
+
+  const mesh = useRef<Mesh>(null)
+
+  const group = useRef<Group>(null)
+
+  const texture = useLoader(TextureLoader, '/image/jupiter/jupiter.jpg')
+
+  useFrame(() => {
+    if (mesh.current) {
+      mesh.current.rotation.y = rotation * time.current
+    }
+
+    if (group.current) {
+      const [x, y, z] = heliocentric(JUPITER, time.current)
+
+      group.current.position.set(toSceneUnit(x), toSceneUnit(y), toSceneUnit(z))
+    }
+  })
+
+  useFocusTarget('jupiter', group, radius)
+
+  const [x, y, z] = heliocentric(JUPITER, time.current)
+
+  const position: Vec = [toSceneUnit(x), toSceneUnit(y), toSceneUnit(z)]
+
+  return (
+    <group
+      ref={group}
+      position={position}
+      rotation={[obliquity, 0, 0]}>
+      <mesh ref={mesh}>
+        <sphereGeometry args={[radius, 96, 64]} />
+        <meshStandardMaterial
+          metalness={0}
+          roughness={0.85}>
+          <primitive
+            attach='map'
+            object={texture}
+            anisotropy={8}
+            colorSpace={SRGBColorSpace}
+          />
+        </meshStandardMaterial>
+      </mesh>
+      <group rotation={[-obliquity, 0, 0]}>
+        <Name
+          id='jupiter'
+          name={JUPITER.name}
+          radius={radius}
+        />
+      </group>
+    </group>
+  )
+}
