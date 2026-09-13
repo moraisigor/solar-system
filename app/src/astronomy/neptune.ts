4d87e036fe15dbfcbf17db5443332f69f1c0ce50
Igor Morais <igor.morais@hollowsoft.com>
2026-09-13 13:34:13 +0000

    add neptune data

---
+++ app/src/astronomy/neptune.ts
@@ -0,0 +1,26 @@
+export const NEPTUNE = {
+  name: 'Neptune',
+  axis: 30.06992276,
+  orbit: 60190.03,
+  radius: 24622,
+  rotation: 0.67125,
+  obliquity: 28.32,
+  inclination: 1.77004347,
+  eccentricity: 0.00859048,
+  ring: {
+    in: 40_900,
+    out: 62_958,
+    list: [
+      { width: 2_000, depth: 0.00008, radius: 41_900 },
+      { width: 113, depth: 0.0062, radius: 53_200 },
+      { width: 4_000, depth: 0.00015, radius: 55_200 },
+      { width: 100, depth: 0.00015, radius: 57_200 },
+      { width: 35, depth: 0.011, radius: 62_933 }
+    ]
+  },
+  longitude: {
+    average: -55.12002969,
+    ascending: 131.78422574,
+    perihelion: 44.96476227
+  }
+} as const
