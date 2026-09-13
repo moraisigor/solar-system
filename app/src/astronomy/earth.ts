d4c8d6806a3ff9142995bbad9608d412883a2aea
Igor Morais <igor.morais@hollowsoft.com>
2026-09-13 14:02:31 +0000

    add earth scene component

---
+++ app/src/astronomy/earth.ts
@@ -0,0 +1,15 @@
+export const EARTH = {
+  name: 'Earth',
+  axis: 1.00000261,
+  orbit: 365.25636,
+  radius: 6371.0084,
+  rotation: 0.99726968,
+  obliquity: 23.439281,
+  inclination: -0.00001531,
+  eccentricity: 0.01671123,
+  longitude: {
+    average: 100.46457166,
+    ascending: 0,
+    perihelion: 102.93768193
+  }
+} as const
---
+++ app/src/scene/Earth/earth.tsx
@@ -0,0 +1,71 @@
+import { useRef, type FunctionComponent } from 'react'
+
+import { useFrame, useLoader } from '@react-three/fiber'
+import { SRGBColorSpace, TextureLoader, type Group, type Mesh } from 'three'
+
+import { useClock } from '@/scene/Clock'
+import { useFocusTarget } from '@/scene/Focus'
+import { Name } from '@/scene/Name'
+
+import { EARTH, heliocentric, toSceneUnit } from '@/astronomy'
+import type { Vec } from '@/astronomy/kepler'
+
+const radius = toSceneUnit(EARTH.radius)
+const rotation = (Math.PI * 2) / EARTH.rotation
+const obliquity = (EARTH.obliquity * Math.PI) / 180
+
+export const Earth: FunctionComponent = () => {
+  const { time } = useClock()
+
+  const mesh = useRef<Mesh>(null)
+
+  const group = useRef<Group>(null)
+
+  const texture = useLoader(TextureLoader, '/textures/earth/earth_daymap_8k.jpg')
+
+  useFrame(() => {
+    if (mesh.current) {
+      mesh.current.rotation.y = rotation * time.current
+    }
+
+    if (group.current) {
+      const [x, y, z] = heliocentric(EARTH, time.current)
+
+      group.current.position.set(toSceneUnit(x), toSceneUnit(y), toSceneUnit(z))
+    }
+  })
+
+  useFocusTarget('earth', group, radius)
+
+  const [x, y, z] = heliocentric(EARTH, time.current)
+
+  const position: Vec = [toSceneUnit(x), toSceneUnit(y), toSceneUnit(z)]
+
+  return (
+    <group
+      ref={group}
+      position={position}
+      rotation={[obliquity, 0, 0]}>
+      <mesh ref={mesh}>
+        <sphereGeometry args={[radius, 96, 64]} />
+        <meshStandardMaterial
+          metalness={0}
+          roughness={0.85}>
+          <primitive
+            attach='map'
+            object={texture}
+            anisotropy={8}
+            colorSpace={SRGBColorSpace}
+          />
+        </meshStandardMaterial>
+      </mesh>
+      <group rotation={[-obliquity, 0, 0]}>
+        <Name
+          id='earth'
+          name={EARTH.name}
+          radius={radius}
+        />
+      </group>
+    </group>
+  )
+}
