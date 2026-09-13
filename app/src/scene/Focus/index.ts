bc9d539900fd64ee96ba5c4b1d386bba4ad39b24
Igor Morais <igor.morais@hollowsoft.com>
2026-09-13 18:27:23 +0000

    export focus component

---
+++ app/src/scene/Focus/index.ts
@@ -0,0 +1,7 @@
+export type { Element } from './element'
+
+export { FocusProvider } from './focus.provider'
+export { Root } from './root'
+export { useFocus } from './use.focus'
+export { useFocusState } from './use.focus.state'
+export { useFocusTarget } from './use.focus.target'
