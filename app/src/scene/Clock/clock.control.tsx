25b5dae2285c196a90efd144b57d13e683df192a
Igor Morais <igor.morais@hollowsoft.com>
2026-09-15 23:57:14 +0000

    update clock control

--- app/src/scene/Clock/clock.control.tsx
+++ app/src/scene/Clock/clock.control.tsx
@@ -1,5 +1,7 @@
 import { useFrame } from '@react-three/fiber'
 
+import { STAGE } from '@/scene/constant'
+
 import { useClock } from './use.clock'
 
 export const ClockControl = () => {
@@ -8,7 +10,7 @@ export const ClockControl = () => {
   useFrame((_, value) => {
     // 1 second is equivalent to 1 hour
     time.current += (1 / 24) * value
-  }, -1)
+  }, STAGE.TIME)
 
   return null
 }
