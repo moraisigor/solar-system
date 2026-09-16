ec02b94ce5426b0b93b1e698453d262fc4cc2b22
Igor Morais <igor.morais@hollowsoft.com>
2026-09-16 00:09:07 +0000

    add astronomy constant

---
+++ app/src/astronomy/constant.ts
@@ -0,0 +1,6 @@
+export const SCENE_UNIT = 1_000
+
+export const ASTRONOMICAL_UNIT = 149_597_870.7
+
+export const SUN_LIGHT_COLOR = '#fff4e5'
+export const SUN_LIGHT_INTENSITY = (ASTRONOMICAL_UNIT / SCENE_UNIT) ** 2
