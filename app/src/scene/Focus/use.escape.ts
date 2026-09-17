8140dd4b69ce16b6bedcd235edb11261b436bf30
Igor Morais <igor.morais@hollowsoft.com>
2026-09-17 11:41:11 -0300

    add use escape hook

---
+++ app/src/scene/Focus/use.escape.ts
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+
+import type { ID } from './id'
+
+const KEY_ESC = 'Escape'
+
+export const useEscape = (focus: (id: ID | null) => void) => {
+  useEffect(() => {
+    const onKeyDown = (event: KeyboardEvent) => {
+      const { key } = event
+
+      switch (key) {
+        case KEY_ESC:
+          event.preventDefault()
+
+          return focus(null)
+      }
+    }
+
+    window.addEventListener('keydown', onKeyDown)
+
+    return () => window.removeEventListener('keydown', onKeyDown)
+  }, [])
+}
