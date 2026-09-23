99a3f87a09801a1d5cc5b670036695d87d9d82c8
Igor Morais <igor.morais@hollowsoft.com>
2026-09-23 17:27:59 -0300

    implement name component

--- app/src/scene/Name/name.tsx
+++ app/src/scene/Name/name.tsx
@@ -1,8 +1,13 @@
-import { useEffect, useMemo, type FunctionComponent } from 'react'
+import { useEffect, useMemo, useRef, type FunctionComponent } from 'react'
 
+import { useFrame } from '@react-three/fiber'
 import { CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js'
 
-import { useFocus, type ID } from '../Focus'
+import { MIN_OPACITY } from '@/scene/constant'
+
+import type { ID } from '@/type'
+
+import { useFocus } from '../Focus'
 
 type NameProps = {
   id: ID
@@ -10,10 +15,14 @@ type NameProps = {
   radius: number
 }
 
-export const Name: FunctionComponent<NameProps> = ({ id, name, radius }) => {
-  const { focus, current } = useFocus()
+const HEIGHT = 1.25
 
-  const object = useMemo(() => {
+export const Name: FunctionComponent<NameProps> = ({ id, name, radius }) => {
+  const { opacity, focus } = useFocus()
+
+  const progress = useRef<number>(-1)
+
+  const { span, object } = useMemo(() => {
     const element = document.createElement('span')
     element.className = 'planet-label-anchor'
 
@@ -23,7 +32,7 @@ export const Name: FunctionComponent<NameProps> = ({ id, name, radius }) => {
 
     element.appendChild(span)
 
-    return new CSS2DObject(element)
+    return { span, object: new CSS2DObject(element) }
   }, [name])
 
   useEffect(() => {
@@ -31,27 +40,31 @@ export const Name: FunctionComponent<NameProps> = ({ id, name, radius }) => {
   }, [object])
 
   useEffect(() => {
-    const element = object.element.querySelector('.planet-label')
+    const onClick = (event: MouseEvent) => {
+      event.preventDefault()
+      event.stopPropagation()
 
-    if (element instanceof HTMLElement) {
-      const onClick = (e: MouseEvent) => {
-        e.preventDefault()
-        e.stopPropagation()
-
-        focus(id)
-      }
-
-      element.addEventListener('click', onClick)
-
-      return () => element.removeEventListener('click', onClick)
+      focus(id)
     }
-  }, [])
+
+    span.addEventListener('click', onClick)
+
+    return () => span.removeEventListener('click', onClick)
+  }, [id, name, focus])
+
+  useFrame(() => {
+    if (opacity.current === progress.current) return
+    progress.current = opacity.current
+
+    object.visible = opacity.current > MIN_OPACITY
+
+    object.element.style.opacity = String(opacity.current)
+  })
 
   return (
     <primitive
       object={object}
-      visible={current === null}
-      position={[0, radius * 1.25, 0]}
+      position={[0, radius * HEIGHT, 0]}
     />
   )
 }
