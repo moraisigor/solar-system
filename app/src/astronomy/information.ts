d85208c0054b00d8febdc440bfec820f3a44a925
Igor Morais <igor.morais@hollowsoft.com>
2026-09-23 01:36:32 -0300

    add information builder

---
+++ app/src/astronomy/information.ts
@@ -0,0 +1,63 @@
+import { UNIT, type ID } from '@/type'
+
+import { degree, distance, time } from '@/astronomy/format'
+
+import { EARTH } from './earth'
+import { JUPITER } from './jupiter'
+import { MARS } from './mars'
+import { MERCURY } from './mercury'
+import { NEPTUNE } from './neptune'
+import { SATURN } from './saturn'
+import { SUN } from './sun'
+import { URANUS } from './uranus'
+import { VENUS } from './venus'
+
+type Format = {
+  format: (e: number, unit: string) => string
+}
+
+type Information = {
+  name: string
+  values: {
+    name: string
+    value: string
+  }[]
+}
+
+const PLANET = {
+  orbit: { format: time },
+  radius: { format: distance },
+  rotation: { format: time },
+  obliquity: { format: degree }
+}
+
+const build = <K extends string>(e: { name: string } & Record<NoInfer<K>, number>, property: Record<K, Format>): Information => {
+  const { name } = e
+
+  const values = Object.keys(property).map((key) => {
+    const { format } = property[key]
+
+    return {
+      name: key,
+      value: format(e[key])
+    }
+  })
+
+  return { name, values }
+}
+
+export const INFORMATION: Record<ID, Information> = {
+  sun: build(SUN, {
+    radius: { format: distance },
+    rotation: { format: time },
+    obliquity: { format: degree },
+  }),
+  earth: build(EARTH, PLANET),
+  jupiter: build(JUPITER, PLANET),
+  mars: build(MARS, PLANET),
+  mercury: build(MERCURY, PLANET),
+  neptune: build(NEPTUNE, PLANET),
+  saturn: build(SATURN, PLANET),
+  uranus: build(URANUS, PLANET),
+  venus: build(VENUS, PLANET)
+}
